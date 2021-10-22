b9623e6428f0f93e2f208b898877a236a17fd3af
nam140401 <nguyennam140401@gmail.com>
2021-10-22 15:45:48 +0700

    update

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,16 @@
 import React, { Fragment, useEffect } from 'react'
-import useSWR from 'swr'
+// import useSWR from 'swr'
 import Carousel from 'react-bootstrap/Carousel'
 import * as api from '../util/api'
 import CategoryBlog from '../components/CategoryBlog/CategoryBlog'
 import Featured from '../components/Featured/Featured'
 import Style from '../styles/style.js'
-const fetcher = (url) =>
-    fetch(url)
-        .then((res) => res.json())
-        .then((res) => console.log(res))
+// const fetcher = (url) =>
+//     fetch(url)
+//         .then((res) => res.json())
+//         .then((res) => console.log(res))
 const Home = ({ carouselState, categoryState }) => {
-    const { data, error } = useSWR('/api/posts', fetcher)
+    // const { data, error } = useSWR('/api/posts', fetcher)
 
     return (
         <Fragment>
